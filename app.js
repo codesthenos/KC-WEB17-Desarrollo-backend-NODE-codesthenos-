783414293e59a57270a1c97d1fa972cbe9807c61
codesthenos <codesthenos@gmail.com>
2024-11-02 16:07:54 +0100

    Scaffolding final del proyecto, comienzo con la app

---
+++ app.js
@@ -0,0 +1,52 @@
+import { join } from 'node:path'
+import express from 'express'
+import createError from 'http-errors'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import indexRouter from './routes/index.js'
+import usersRouter from './routes/users.js'
+
+const app = express()
+
+// view engine setup
+app.set('views', join(import.meta.dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// middlewares
+
+// morgan logger for http requests logs
+app.use(logger('dev'))
+// transforms json objects into js objects
+app.use(express.json())
+// transforms data sent by a form to a js object
+app.use(express.urlencoded({ extended: false }))
+// cookie parser to get cookies from client
+app.use(cookieParser())
+// set the folder where statis resources will be served
+app.use(express.static(join(import.meta.dirname, 'public')))
+
+// Routing
+
+// homepage
+app.use('/', indexRouter)
+// user page
+app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+export default app
