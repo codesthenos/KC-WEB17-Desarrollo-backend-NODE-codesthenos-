0bed9a0b605f3d93e3a1a794c73525ea34cca882
codesthenos <codesthenos@gmail.com>
2024-11-06 01:52:56 +0100

    Creado el registro con login automatico, manejo de errores y toda la pesca, creo que no me dejo nada

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,4 +1,8 @@
+import { z } from 'zod'
 import { setLocals, REGISTER_TITLE as title } from '../lib/config.js'
+import { loginSchema } from '../lib/validatorSchemas.js'
+import { User } from '../models/User.js'
+import { handleLoginValidationError } from '../lib/zodErrorHandlers.js'
 
 export const getRegister = (req, res) => {
   if (req.session.userId) return res.redirect('/')
@@ -6,4 +10,37 @@ export const getRegister = (req, res) => {
   res.render('login-register')
 }
 
-export const postRegister = () => {}
+export const postRegister = async (req, res, next) => {
+  try {
+    // get form data
+    const { email, password } = req.body
+    // validate for data
+    loginSchema.parse(req.body)
+    // normalize email
+    const normalizedEmail = email.toLowerCase().trim()
+    // check if there is a REGISTERED user in the MongoDB
+    const user = await User.findOne({ email: normalizedEmail })
+    if (user) {
+      setLocals(res, { title, email, error: '   ERROR: This email is already REGISTERED' })
+      res.render('login-register')
+      return
+    }
+    // create new User with hashed pass
+    const newUser = new User({
+      email: normalizedEmail,
+      password: await User.hashPassword(password)
+    })
+    // store it in the database, and in a variable to automatically login the user
+    const savedUser = await newUser.save()
+    // automatically login
+    req.session.userId = savedUser._id
+    req.session.email = savedUser.email
+    res.redirect('/')
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      handleLoginValidationError(error, res, req.body.email)
+    } else {
+      next(error)
+    }
+  }
+}
