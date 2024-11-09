d475d83292f97207be86b14c29c17463dd3f5e27
codesthenos <codesthenos@gmail.com>
2024-11-09 13:22:01 +0100

    Al final me he liado y le he metido 5 horitas al filtro del precio

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,4 @@
-import { APP_TITLE, CURRENT_PAGE, validateQueryPrice, normalizePriceMongo, validateQuerySort, normalizeSortMongo, validateQueryTag, normalizeURL, PRODUCTS_PER_PAGE, setLocals } from '../lib/config.js'
+import { APP_TITLE, CURRENT_PAGE, normalizePriceMongo, validateQuerySort, normalizeSortMongo, validateQueryTag, normalizeURL, PRODUCTS_PER_PAGE, setLocals, validateQueryPrice, normalizedPrice } from '../lib/config.js'
 import { Product } from '../models/Product.js'
 
 const index = async (req, res, next) => {
@@ -20,20 +20,54 @@ const index = async (req, res, next) => {
     // get userID from session
     const owner = req.session.userId
     // get query params
-    const { limit, skip, sort, name, price, tag } = req.query
-    // init filters and option for the list of products
-    const filters = { owner }
+    const { limit, skip, sort, name, price, tag, priceMin, priceMax, priceExact } = req.query
+    // init options for sorting and pagination
     const options = {}
+    // Generating URLS
+    const currentParams = { skip, limit, name, price, tag, sort, priceMin, priceMax, priceExact }
+    // sorting nav locals if user logged
+    res.locals.hrefNameSortAtoZ = normalizeURL({ ...currentParams, sort: 'name' })
+    res.locals.hrefNameSortZtoA = normalizeURL({ ...currentParams, sort: 'name-1' })
+    // sorting price nav locals
+    res.locals.hrefPriceSortASC = normalizeURL({ ...currentParams, sort: 'price' })
+    res.locals.hrefPriceSortDES = normalizeURL({ ...currentParams, sort: 'price-1' })
+    // unsort nav local
+    res.locals.hrefUnsort = normalizeURL({ ...currentParams, sort: '' })
+    // locals for UNSET filters
+    res.locals.hrefNameUnsetFilter = normalizeURL({ ...currentParams, name: '' })
+    res.locals.hrefPriceMinMaxUnsetFilter = normalizeURL({ ...currentParams, priceMin: '', priceMax: '' })
+    res.locals.hrefPriceExactUnsetFilter = normalizeURL({ ...currentParams, priceExact: '' })
+    res.locals.hrefTagUnsetFilter = normalizeURL({ ...currentParams, tag: '' })
+    // PAGINATION behaviour if we got in url only one, skip or limit
+    if ((!limit && skip) || (limit && !skip)) {
+      res.redirect(normalizeURL({ ...currentParams, skip: undefined, limit: undefined }))
+      return
+    }
+    // PAGINATION behaviour if we got in url skip and limit
+    if (skip && limit) {
+      const normalizedLimit = Math.abs(+limit)
+      options.skip = skip
+      options.limit = normalizedLimit
+      res.locals.skip = skip
+      res.locals.limit = normalizedLimit
+      // next page button
+      res.locals.nextPage = +skip + normalizedLimit
+      res.locals.hrefNextPage = normalizeURL({ ...currentParams, skip: res.locals.nextPage, limit: normalizedLimit })
+      // previous page button
+      res.locals.previousPage = +skip - normalizedLimit < 0 ? 0 : +skip - normalizedLimit
+      res.locals.hrefPreviousPage = normalizeURL({ ...currentParams, skip: res.locals.previousPage, limit: normalizedLimit })
+      // show all button
+      res.locals.hrefShowAll = normalizeURL({ name, price, tag, sort, priceMin, priceMax, priceExact })
+    }
+    // 3 by 3 button     PAGINATION locals.hrefPaginate if dont have either skip and limit
+    res.locals.hrefPaginate = normalizeURL({ ...currentParams, skip: CURRENT_PAGE, limit: PRODUCTS_PER_PAGE })
+    // init filters
+    const filters = { owner }
     // declare list filters, options and locals with validation
     // name
     if (name) {
       filters.name = new RegExp(`^${name}`, 'i')
       res.locals.name = name
-    }
-    // price
-    if (price) {
-      res.locals.price = validateQueryPrice(price)
-      filters.price = normalizePriceMongo(price)
     }
     // tag
     if (tag) {
@@ -46,43 +80,26 @@ const index = async (req, res, next) => {
       res.locals.sort = validateQuerySort(sort)
       options.sort = normalizeSortMongo(sort)
     }
-    // Generating URLS
-    const currentParams = { skip, limit, name, price, tag, sort }
-    // sorting nav locals if user logged
-    res.locals.hrefNameSortAtoZ = normalizeURL({ ...currentParams, sort: 'name' })
-    res.locals.hrefNameSortZtoA = normalizeURL({ ...currentParams, sort: 'name-1' })
-    // sorting price nav locals
-    res.locals.hrefPriceSortASC = normalizeURL({ ...currentParams, sort: 'price' })
-    res.locals.hrefPriceSortDES = normalizeURL({ ...currentParams, sort: 'price-1' })
-    // unsort nav local
-    res.locals.hrefUnsort = normalizeURL({ ...currentParams, sort: '' })
-    // locals for UNSET filters
-    res.locals.hrefNameUnsetFilter = normalizeURL({ ...currentParams, name: '' })
-    res.locals.hrefPriceUnsetFilter = normalizeURL({ ...currentParams, price: '' })
-    res.locals.hrefTagUnsetFilter = normalizeURL({ ...currentParams, tag: '' })
-    // PAGINATION behaviour if we got in url only one, skip or limit
-    if ((!limit && skip) || (limit && !skip)) {
-      res.redirect(normalizeURL({ name, price, tag, sort }))
+    // price If someone add the query param 'price' manually to the url, it will delete the priceMin/Max?exact if exists and use only price query param to filter
+    if (price && (priceMin || priceMax || priceExact)) {
+      res.redirect(normalizeURL({ ...currentParams, priceMin: '', priceMax: '', priceExact: '' }))
       return
     }
-    // PAGINATION behaviour if we got in url skip and limit
-    if (skip && limit) {
-      const normalizedLimit = Math.abs(+limit)
-      options.skip = skip
-      options.limit = normalizedLimit
-      res.locals.skip = skip
-      res.locals.limit = normalizedLimit
-      // next page button
-      res.locals.nextPage = +skip + normalizedLimit
-      res.locals.hrefNextPage = normalizeURL({ skip: res.locals.nextPage, limit: normalizedLimit, name, price, tag, sort })
-      // previous page button
-      res.locals.previousPage = +skip - normalizedLimit < 0 ? 0 : +skip - normalizedLimit
-      res.locals.hrefPreviousPage = normalizeURL({ skip: res.locals.previousPage, limit: normalizedLimit, name, price, tag, sort })
-      // show all button
-      res.locals.hrefShowAll = normalizeURL({ name, price, tag, sort })
+    if (price) {
+      const validatedPrice = validateQueryPrice(price)
+      res.locals.price = validatedPrice
+      res.locals.priceView = validatedPrice
+      filters.price = normalizePriceMongo(validatedPrice)
     }
-    // 3 by 3 button     PAGINATION locals.hrefPaginate if dont have either skip and limit
-    res.locals.hrefPaginate = normalizeURL({ skip: CURRENT_PAGE, limit: PRODUCTS_PER_PAGE, name, price, tag, sort })
+    if (!price && (priceMin || priceMax || priceExact)) {
+      res.locals.priceMin = priceMin
+      res.locals.priceMax = priceMax
+      res.locals.priceExact = priceExact
+      const validatedPrice = normalizedPrice({ priceMin, priceMax, priceExact })
+      res.locals.priceView = validatedPrice
+      filters.price = normalizePriceMongo(normalizedPrice({ priceMin, priceMax, priceExact }))
+    }
+
     // total products length stored in a local
     res.locals.productsLength = await Product.countDocuments(filters)
     // list of products stored in a local
@@ -102,7 +119,8 @@ const index = async (req, res, next) => {
         hrefPriceSortDES: normalizeURL({ ...req.query, sort: 'price-1' }),
         hrefUnsort: normalizeURL({ ...req.query, sort: '' }),
         hrefNameUnsetFilter: normalizeURL({ ...req.query, name: '' }),
-        hrefPriceUnsetFilter: normalizeURL({ ...req.query, price: '' }),
+        hrefPriceMinMaxUnsetFilter: normalizeURL({ ...req.query, priceMin: '', priceMax: '' }),
+        hrefPriceExactUnsetFilter: normalizeURL({ ...req.query, priceExact: '' }),
         hrefTagUnsetFilter: normalizeURL({ ...req.query, tag: '' }),
         nextPage: +req.query.skip + Math.abs(+req.query.limit),
         hrefNextPage: normalizeURL({ ...req.query, skip: +req.query.skip + Math.abs(+req.query.limit), limit: Math.abs(+req.query.limit) }),
@@ -110,7 +128,32 @@ const index = async (req, res, next) => {
         hrefPreviousPage: normalizeURL({ ...req.query, skip: +req.query.skip - Math.abs(+req.query.limit) < 0 ? 0 : +req.query.skip - Math.abs(+req.query.limit), limit: Math.abs(+req.query.limit) }),
         hrefShowAll: normalizeURL({ name: req.query.name, price: req.query.price, tag: req.query.tag, sort: req.query.sort }),
         hrefPaginate: normalizeURL({ ...req.query, skip: CURRENT_PAGE, limit: PRODUCTS_PER_PAGE }),
-        error: 'ERROR IN PRICE FILTER\n\nPRICE FILTER MUST MATCH ONE OF THE FOLLOWING PATTERNS\n    <number> | <number>- | -<number> | <number>-<number>'
+        errorURL: 'ERROR IN PRICE FILTER\n\nPRICE FILTER MUST MATCH ONE OF THE FOLLOWING PATTERNS\n    <number> | <number>- | -<number> | <number>-<number>'
+      })
+      res.render('home')
+      return
+    } else if (error.message === '400 BAD REQUEST | ERROR PRICE: FAILED ON: PRICE MIN >= 0 | PRICE MAX > 0 | PRICE EXACT > 0') {
+      setLocals(res, {
+        title: APP_TITLE,
+        headerLinkHref: '/logout',
+        headerLinkText: 'LOGOUT',
+        ...req.query,
+        hrefNameSortAtoZ: normalizeURL({ ...req.query, sort: 'name' }),
+        hrefNameSortZtoA: normalizeURL({ ...req.query, sort: 'name-1' }),
+        hrefPriceSortASC: normalizeURL({ ...req.query, sort: 'price' }),
+        hrefPriceSortDES: normalizeURL({ ...req.query, sort: 'price-1' }),
+        hrefUnsort: normalizeURL({ ...req.query, sort: '' }),
+        hrefNameUnsetFilter: normalizeURL({ ...req.query, name: '' }),
+        hrefPriceMinMaxUnsetFilter: normalizeURL({ ...req.query, priceMin: '', priceMax: '' }),
+        hrefPriceExactUnsetFilter: normalizeURL({ ...req.query, priceExact: '' }),
+        hrefTagUnsetFilter: normalizeURL({ ...req.query, tag: '' }),
+        nextPage: +req.query.skip + Math.abs(+req.query.limit),
+        hrefNextPage: normalizeURL({ ...req.query, skip: +req.query.skip + Math.abs(+req.query.limit), limit: Math.abs(+req.query.limit) }),
+        previousPage: +req.query.skip - Math.abs(+req.query.limit) < 0 ? 0 : +req.query.skip - Math.abs(+req.query.limit),
+        hrefPreviousPage: normalizeURL({ ...req.query, skip: +req.query.skip - Math.abs(+req.query.limit) < 0 ? 0 : +req.query.skip - Math.abs(+req.query.limit), limit: Math.abs(+req.query.limit) }),
+        hrefShowAll: normalizeURL({ name: req.query.name, price: req.query.price, tag: req.query.tag, sort: req.query.sort }),
+        hrefPaginate: normalizeURL({ ...req.query, skip: CURRENT_PAGE, limit: PRODUCTS_PER_PAGE }),
+        error: 'ERROR PRICE:\n\n    FAILED ON: PRICE MIN >= 0 | PRICE MAX > 0 | PRICE EXACT > 0'
       })
       res.render('home')
       return
